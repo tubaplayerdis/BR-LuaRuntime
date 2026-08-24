3f7e6e57fe2a6ca56c0294eb37ace7f084355088
Aaron Wilk <aaron.wilk@icloud.com>
2026-08-24 13:24:08 -0500

    Remove MathLib reference

--- Examples/TESTING.lua
+++ Examples/TESTING.lua
@@ -1,8 +1,6 @@
 --=TESTING
 -- Include the above at the top of your TextBrick text, without the comment prefix "--"
 
-local MathLib = require("MathLib")
-
 --This function HAS to be implemented globally in Execution modules
 function Tick(Delta)
 	local X = GetInChannelVal(0) --Reads the bound brick on index 0 output channel value
@@ -10,7 +8,7 @@ function Tick(Delta)
 	local XZ = GetInChannelVal(-1) --Reads this bricks input channel
 	print("X" .. X)
 	print("Z" .. Z)
-	SetOutChannelVal(Delta * MathLib.PI) --Sets this bricks output channel value
+	SetOutChannelVal(Delta) --Sets this bricks output channel value
 	SetOutChannelValNamed("LastDeltaTime", Delta) --Looks for a switch brick named "LastDeltaTime" and sets its output channel to Delta
 	local LastDeltaTime = GetOutChannelValNamed("LastDeltaTime") --Looks for s switch brick named "LastDeltaTime" and reads its output channel value
 	print(LastDeltaTime)
