7832ee6ce06a92ac8f2f9293840010652a5d690d
Aaron Wilk <aaron.wilk@icloud.com>
2026-09-02 23:31:11 -0400

    Delete Examples/In.lua

--- Examples/In.lua
+++
@@ -1,9 +0,0 @@
---In Define these globally?
-local In = {}
-    function In.Get(Index)
-        return GetInChannelVal(Index)
-    end
-    function In.Set(Index, Value)
-        SetInChannelVal(Index, Value)
-    end
-return In
