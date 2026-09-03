421568d9db1741c1ef49aa74c5814bccbb84216e
Aaron Wilk <aaron.wilk@icloud.com>
2026-09-02 23:31:04 -0400

    Delete Examples/Out.lua

--- Examples/Out.lua
+++
@@ -1,9 +0,0 @@
---Out Define these globally?
-local Out = {}
-    function Out.Get()
-        return GetOutChannelVal()
-    end
-    function Out.Set(Value)
-        SetOutChannelVal(Value)
-    end
-return Out
